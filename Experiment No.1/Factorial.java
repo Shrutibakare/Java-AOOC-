6383c62a80ba33c7febac3c52dd6387c6cd35d2c
Shruti <shruti.bakare31@gmail.com>
2026-02-14 17:20:58 +0530

    Update Factorial.java

--- Experiment No.1/Factorial.java
+++ Experiment No.1/Factorial.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
-
-class Factorial {
-   Factorial() {
-   }
-
-   public static void main(String[] var0) {
-      Scanner var1 = new Scanner(System.in);
-      long var3 = 1L;
-      System.out.print("Enter a number: ");
-      int var2 = var1.nextInt();
-
-      for(int var5 = 1; var5 <= var2; ++var5) {
-         var3 *= (long)var5;
-      }
-
-      System.out.println("Factorial of " + var2 + " = " + var3);
-      var1.close();
-   }
+class fact{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter number:");
+        int num = sc.nextInt();
+        int factorial = 1;
+        for(int i =1;i<=num;i++){
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of num is:"+factorial);
+    }
 }
