3169001bd663efa3c9ac47a14db1e2dba52bbba3
Shruti <shruti.bakare31@gmail.com>
2026-02-16 23:07:09 +0530

    Refactor MergeArrays to take user input for sizes

--- Experiment No.1/MergeArrays.java
+++ Experiment No.1/MergeArrays.java
@@ -1,26 +1,37 @@
-class MergeArrays {
-    public static void main(String args[]) {
-        int a[] = {1, 3, 5, 7};
-        int b[] = {2, 4, 6, 8};
-        int c[] = new int[a.length + b.length];
+import java.util.Scanner;
+ class main{
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         int n1,n2;
+         System.out.println("Enter size of arr1");
+         n1 = sc.nextInt();
+         System.out.println("Enter size of arr2");
+         n2 = sc.nextInt();
+         
+         int a[] = new int[n1];
+         int b[] = new int[n2];
+         int c[] = new int[n1+n2];
+         
+         System.out.println("Enter elements in 1st array:");
+         for(int i =0; i< n1;i++){
+             a[i] =sc.nextInt();
+         }
+         
+          System.out.println("Enter elements in 2nd array:");
+         for(int i =0; i< n2;i++){
+             b[i] =sc.nextInt();
+         }
+         for(int i =0;i<n1;i++){
+             c[i] = a[i];
+         }
+         for(int i =0;i<n2;i++){
+             c[n1+i] = b[i];
+         }
+          System.out.println("Merged Array:");
+        for(int i = 0; i < n1 + n2; i++)
+            System.out.print(c[i] + " ");
 
-        int i = 0, j = 0, k = 0;
-
-        while (i < a.length && j < b.length) {
-            if (a[i] < b[j])
-                c[k++] = a[i++];
-            else
-                c[k++] = b[j++];
-        }
-
-        while (i < a.length)
-            c[k++] = a[i++];
-
-        while (j < b.length)
-            c[k++] = b[j++];
-
-        System.out.println("Merged Array:");
-        for (int x : c)
-            System.out.print(x + " ");
-    }
-}
+        sc.close();
+     }
+     
+ }
